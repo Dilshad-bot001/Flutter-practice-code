af45fb45c1643df635d3b05432e53dbef6766989
Dilshad <dilshadahmad92425@gmail.com>
2022-02-21 03:07:22 +0530

    adding images,button, input buttons

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,14 +6,65 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("Login page",
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 20,
-          color: Colors.blue,
-        ),),
-      ),
+      child: Column(
+        children: [
+          Image.asset("assests/images/first.jpg",
+          fit: BoxFit.cover,),
+
+          SizedBox(
+            height: 20.0,
+            // child: Text("hello"),
+          ),
+
+          Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+
+          SizedBox(
+            height: 20.0,
+            // child: Text("hello"),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical:16.0, horizontal: 32.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter Username",
+                    labelText: "Username"
+                  ),
+                ),
+
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter Password",
+                    labelText: "Password"
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          SizedBox(
+            height: 20.0,
+          ),
+
+          ElevatedButton(onPressed: () {
+            print("Welcome sir ji");
+          },
+           child: Text("Login"),
+           style: TextButton.styleFrom(),
+
+           )
+
+        ],
+      )
     );
   }
 }
